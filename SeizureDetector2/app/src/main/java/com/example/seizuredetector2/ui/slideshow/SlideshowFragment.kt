c760dd0ae7fcc29bff2825441e398dc0a43e364d
Vikram Bala <vikrambala.va@gmail.com>
2022-02-06 02:10:49 -0500

    Fixed Firebase, UI updates

--- SeizureDetector2/app/src/main/java/com/example/seizuredetector2/ui/slideshow/SlideshowFragment.kt
+++ SeizureDetector2/app/src/main/java/com/example/seizuredetector2/ui/slideshow/SlideshowFragment.kt
@@ -1,7 +1,16 @@
 package com.example.seizuredetector2.ui.slideshow
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.Typeface
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +18,12 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.example.seizuredetector2.R
 import com.example.seizuredetector2.databinding.FragmentSlideshowBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -19,17 +31,21 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class SlideshowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
     private lateinit var database: DatabaseReference
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("SimpleDateFormat")
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -46,9 +62,24 @@ class SlideshowFragment : Fragment() {
 //            textView.text = it
 //        }
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+        //get current time of program instance running
+        val c = Calendar.getInstance()
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val strDate: String = sdf.format(c.time)
+        //Log.d(TAG, strDate)
+
+        //firebase integration
         database = Firebase.database.reference
 
         val linearLayout = root.findViewById(R.id.ll) as LinearLayout
+
+        val loggingTextView = TextView(activity)
+        loggingTextView.setText("Log:")
+        loggingTextView.setTypeface(loggingTextView.getTypeface(), Typeface.BOLD)
+        linearLayout.addView(loggingTextView)
+
         var mySet = mutableSetOf<String>()
 
         //val linearLayout = LinearLayout(activity)
@@ -58,17 +89,63 @@ class SlideshowFragment : Fragment() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 // Get Post object and use the values to update the UI
-                val post = dataSnapshot.getValue<HashMap<String, String>>() // hashmap
+                val post = dataSnapshot.getValue<HashMap<String, HashMap<String, String>>>()
+                val statusToCondMap = mapOf("2" to "Panic Attack", "1" to "Seizure")
 
                 if (post != null) {
-                    Log.d(TAG, post.toString())
-                    for ((key, value) in post) {
+                    var sortedPost = TreeMap<String, String>()
+
+                    // build treemap, automatically sorted by time (ISO 8601)
+                    for ((key, map) in post) {
                         if (!mySet.contains(key)) {
                             mySet.add(key)
-                            val textView = TextView(activity)
-                            textView.setText("$key: $value")
-                            println("$key = $value")
-                            linearLayout.addView(textView);
+                            for ((time, status) in map) {
+                                sortedPost[time] = status
+                            }
+                        }
+                    }
+
+                    for ((time, status) in sortedPost) {
+                        val textView = TextView(activity)
+
+                        val cond = statusToCondMap[status]
+
+                        textView.setText("$time: $cond")
+                        if (status == "1") {
+                            textView.setTextColor(Color.RED)
+                        }
+                        linearLayout.addView(textView);
+
+//                        Log.d(TAG, time + ", " + status)
+//                        Log.d(TAG, time.compareTo(strDate).toString())
+                        if (time.compareTo(strDate) > 0) {
+                            when(status) {
+                                "1" -> {  // seizure
+                                    // onClick(binding.root, "seizure")
+
+                                }
+                                "2" -> {  // panic attack
+                                    // onClick(binding.root, "panic attack")
+
+                                    // move this code to seizure later on
+
+//                                    activity!!.supportFragmentManager.popBackStack(null,
+//                                        FragmentManager.POP_BACK_STACK_INCLUSIVE)
+//
+//                                    activity!!.supportFragmentManager.beginTransaction()
+//                                        .setReorderingAllowed(true)
+//                                        .replace(
+//                                            R.id.thingy,
+//                                            HomeFragment(),
+//                                            "findThisFragment"
+//                                        )
+//                                        .addToBackStack(null)
+//                                        .commit()
+                                }
+                                else -> {
+                                    Log.e(TAG, "Error with Firebase retrieval")
+                                }
+                            }
                         }
                     }
                 }
@@ -87,5 +164,55 @@ class SlideshowFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // quick and dirty, not ideal
+    fun onClick(v: View, type: String) {
+
+        // text others
+        val smsManager: SmsManager = SmsManager.getDefault()
+
+        // ideally pull this list from gallery fragment
+        var phonies = mutableListOf("7036180767") //, "4438327344", "9732166660", "5713869265")
+
+        // location
+        if (ActivityCompat.checkSelfPermission(binding.root.context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(binding.root.context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(activity, "First enable LOCATION ACCESS in settings.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                if (location == null) {
+                    Log.d(TAG,"Location not found.")
+                }
+                else {
+                    var msg = "Hi, this is Vikram. I'm currently having a " + type + ". My location " +
+                            "coordinates are: " + location.latitude.toString() + ", " + location.longitude.toString() + "."
+
+                    var geocoder = Geocoder(binding.root.context, Locale.getDefault())
+
+                    var addresses = geocoder.getFromLocation(
+                        location.latitude,
+                        location.longitude,
+                        1
+                    )
+                    val address: String = addresses.get(0)
+                        .getAddressLine(0)
+
+                    var addressMsg = " My estimated address is: $address."
+
+                    for (number in phonies) {
+                        smsManager.sendMultipartTextMessage(number, null,
+                            arrayListOf(msg, addressMsg),
+                            null, null)
+                    }
+                }
+            }
+
+        // code to call phone
+//        val intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "4438327344"))
+//        startActivity(intent)
     }
 }
